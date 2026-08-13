ea2f421705ca893f8dd793c5e1747231b47bcc46
kelai141 <17765135474@qq.com>
2026-08-14 07:36:45 +0800

    chore: build/ 重命名为 build-scripts/（构建配置与产物目录区分）

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,3 +1,4 @@
-import { clientBundle } from './build/tsdown.client.ts'
+﻿import { clientBundle } from './build-scripts/tsdown.client.ts'
 
 export default clientBundle('@dsh-android/dsh-client-ui-responsive', ['lib/types/index.js', 'lib/types/invariant.js'])
+
